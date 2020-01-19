315d682d7bd545558b77fbb8de2a92256072d6dd
hz15041151 <lengjinhua@huize.com>
2020-01-19 10:49:41 +0800

    feat:update doc

--- MyAntd/Form/FormItem.jsx
+++ MyAntd/Form/FormItem.jsx
@@ -1,84 +1,14 @@
-import React, { PureComponent } from 'react';
-import { Form, Input as AntdInput } from 'antd';
-import PropTypes from 'prop-types';
-import styles from './FormItem.less';
+import React, { PureComponent } from 'react'
+import { Form } from 'antd'
+import styles from './FormItem.less'
 
-class Input extends PureComponent {
-  static propTypes = {
-    form: PropTypes.object.isRequired,
-    name: PropTypes.string.isRequired,
-    type: PropTypes.string,
-    label: PropTypes.string,
-    placeholder: PropTypes.string,
-    required: PropTypes.bool,
-    className: PropTypes.string,
-    message: PropTypes.string,
-    defaultValue: PropTypes.any,
-    showLabel: PropTypes.bool,
-    hasFeedback: PropTypes.bool,
-    allowClear: PropTypes.bool,
-    pattern: PropTypes.object,
-    transform: PropTypes.func,
-    formItemLayout: PropTypes.object,
-    rules: PropTypes.array,
-    isPlaceLabel: PropTypes.bool,
-    append: PropTypes.any,
-  };
-
-  static defaultProps = {
-    // name: null,
-    // form: null,
-    label: ' ',
-    message: '',
-    type: 'text',
-    className: null,
-    placeholder: '请输入',
-    formItemLayout: null,
-    defaultValue: undefined,
-    transform: item => (item && item.trim ? item.trim() : item),
-    pattern: null,
-    required: true,
-    allowClear: false,
-    showLabel: true,
-    hasFeedback: true,
-    rules: [],
-    isPlaceLabel: true,
-    append: '',
-  };
-
+class FormItem extends PureComponent {
   render() {
-    const {
-      children,
-      className,
-      // required,
-      // defaultValue,
-      // message = '请输入合法的内容',
-      // pattern,
-      form,
-      // name,
-      // type,
-      label,
-      // allowClear = true,
-      formItemLayout,
-      hasFeedback,
-      style,
-      // placeholder,
-      showLabel,
-      // rules,
-      // transform,
-      // isPlaceLabel,
-      append,
-      // setRef,
-      // disabled,
-      // ...rest
-    } = this.props;
-    // const msg = isPlaceLabel ? placeholder + label : placeholder;
+    const { children, className, form, label, formItemLayout, hasFeedback, style, showLabel, append } = this.props
     if (!form) {
-      return <>控件缺少form属性</>;
+      return <>控件缺少form属性</>
     }
-    // if (!name) {
-    //   return <>控件缺少name属性</>;
-    // }
+
     return (
       <Form.Item
         className={`${className} ${styles.item}`}
@@ -90,10 +20,8 @@ class Input extends PureComponent {
         {children}
         {append}
       </Form.Item>
-    );
+    )
   }
 }
-Input.Search = AntdInput.Search;
-Input.Group = AntdInput.Group;
 
-export default Input;
+export default FormItem
--- README.md
+++ README.md
@@ -1,7 +1,69 @@
 # my-antd
 
-> import {Input} from 'my-antd'
 
-> const {form}=this.props;
+```
+import React from 'react';
+import { PageHeaderWrapper } from '@ant-design/pro-layout';
+import { Card } from 'antd';
+import { Form, Input, Button, FormItem } from 'my-antd';
+// import styles from './Welcome.less';
 
-> <Input form={form} name={name} />
+@Form.create()
+class App extends React.Component {
+  sumbit() {
+    const { form } = this.props;
+    // return promise object
+    return new Promise((resolve, reject) => {
+      form.validateFields((err, values) => {
+        if (err) {
+          reject();
+        } else {
+          // eslint-disable-next-line no-console
+          console.log(values);
+          resolve();
+        }
+      });
+    });
+  }
+
+  render() {
+    const { form } = this.props;
+    const formItemLayout = {
+      labelCol: {
+        span: 4,
+      },
+      wrapperCol: {
+        span: 20,
+      },
+    };
+    return (
+      <PageHeaderWrapper>
+        <Card>
+          <Input
+            form={form}
+            name="name"
+            label="Name"
+            defaultValue="test"
+            placeholder="Enter "
+            formItemLayout={formItemLayout}
+            hasFeedback={false}
+          />
+          <Input
+            form={form}
+            name="remark"
+            label="remark"
+            placeholder="Enter "
+            formItemLayout={formItemLayout}
+            required={false}
+          />
+          <FormItem form={form} formItemLayout={formItemLayout}>
+            <Button onClick={() => this.sumbit()}>Submit</Button>
+          </FormItem>
+        </Card>
+      </PageHeaderWrapper>
+    );
+  }
+}
+export default App;
+
+```
